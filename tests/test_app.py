4edee320a78b56da274f8de143a40bffe6df6a6a
RamaMoe <198829122+RamaMoe@users.noreply.github.com>
2025-11-10 23:04:50 +0200

    Added Jenkinsfile

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,14 @@
 import unittest
 from app import greet
+import os
+import sys
 
+# Add the project root (folder that contains app.py) to Python path
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 class TestApp(unittest.TestCase):
 
-    
+
     def test_greet(self):
         self.assertEqual(greet("World"), "Hello, World!")
 
